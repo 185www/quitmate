da5345efd0bd7f99e0c6b6947055be8f68390706
童 <104879576+185www@users.noreply.github.com>
2026-06-19 21:14:28 +0800

    test: 删除默认模板 widget_test.dart

--- apps/mobile/test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('smoke test - app can be imported', () {
-    // Verify that the app module can be loaded without crashing
-    expect(true, isTrue);
-  });
-}
